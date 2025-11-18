d9d59636e946dcc8c34f78df35ce343352127127
kamiichii <berkay.kilic.ce@gmail.com>
2025-11-18 11:14:13 +0300

    Added a new table

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,4 +1,5 @@
-import { pgTable, timestamp, uuid, text } from "drizzle-orm/pg-core";
+
+import { pgTable, timestamp, uuid, text, unique } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom().notNull(),
@@ -21,3 +22,18 @@ export const feeds = pgTable("feeds",{
   url: text("url").notNull().unique(),
   userId:uuid("user_id").notNull().references(() => users.id, { onDelete: "cascade" }),
 });
+
+export const feedFollows = pgTable("feed_follows",{
+  id: uuid("id").primaryKey().defaultRandom().notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+  userId:uuid("user_id").notNull().references(() => users.id, { onDelete: "cascade" }),
+  feedId:uuid("feed_id").notNull().references(() => feeds.id,{ onDelete:"cascade" }),  
+},
+(t) => [
+  unique().on(t.feedId,t.userId),
+]
+)
